7c57f486fc602127d4d99d4033bd81dcbbb9bfdd
Noam Licht <47696687+noaml13@users.noreply.github.com>
2021-03-07 09:28:10 +0200

    Update Noam.cpp

--- Noam.cpp
+++ Noam.cpp
@@ -1,21 +1,20 @@
+// from https://github.com/noaml13/Haifa-ICPC-Notebook/blob/master/Noam.cpp
 #include <bits/stdc++.h>
+using namespace std;
 #define int int64_t
-#define ii pair<int, int>
-#define x first
-#define y second
-#define vi vector<int>
-#define vvi vector<vi>
-#define vii vector<ii>
-#define vvii vector<vii>
-#define vb vector<bool>
-#define vvb vector<vb>
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ii> vii;
+typedef vector<vii> vvii;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
 #define pb push_back
 #define loop(i,s,e) for(int i=(s);i<(e);i++)
 #define loopr(i,s,e) for(int i=(e)-1;i>=(s);i--)
 #define chkmax(a,b) a = max(a,b)
-#define chkmin(a,b) a=min(a,b)
+#define chkmin(a,b) a = min(a,b)
 #define all(x) x.begin(),x.end()
-using namespace std;
 const int INF = 1e18, MOD = 1e9 + 7;
 
 template <class A, class B> pair<A,B> operator+(pair<A,B>& a, pair<A,B>& b) {
@@ -27,12 +26,16 @@ template <class A, class B> istream& operator>>(istream& is, pair<A,B>& a) {
 template <class A, class B> ostream& operator << (ostream& os, const pair<A,B>& a) {
     return os << a.x << ' ' << a.y; }
 
+template<class T> ostream &operator<<(ostream &os, vector<T> v) { os << '['; if (!v.empty()) { os << v[0]; loop(i, 1, v.size()) os << ',' << v[i]; } return os << ']'; }
+template<class T> ostream &operator<<(ostream &os, set<T> v) { os << '{'; if (!v.empty()) { os << *v.begin(); for(auto it = ++v.begin(); it != v.end(); ++it) os << ',' << *it; } return os << '}'; }
+
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 auto DIST = uniform_int_distribution<int>(0, INF);
 
 
 int32_t main(){
     ios_base::sync_with_stdio(false); cin.tie(0);
+    int n; 
 
     return 0;
 }
