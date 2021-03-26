7b7df5c974e2cdf573ff6ff7f456ec6d61cf1c90
Noam Licht <47696687+noaml13@users.noreply.github.com>
2021-03-26 15:43:38 +0300

    Delete CP/APIO directory

--- CP/APIO/2020/day 1/fun/fun.h
+++
@@ -1,7 +0,0 @@
-#include <vector>
-
-int hoursRequired(int X, int Y);
-
-int attractionsBehind(int X, int Y);
-
-std::vector<int> createFunTour(int N, int Q);
--- CP/APIO/2020/practice/party.h
+++
@@ -1,5 +0,0 @@
-#include <vector>
-
-std::vector<int> ask(std::vector<int> G);
-
-int findUnusualGuest(int N, int M, int Q);
